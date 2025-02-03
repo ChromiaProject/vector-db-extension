c2547caa48870e750c77b05888bb461a848d41e2
Johan Nilsson <johan.nilsson@chromaway.com>
2025-02-03 09:01:31 +0100

    Review fixes

--- src/main/kotlin/net/postchain/vectordb/VectorDbEventProcessor.kt
+++ src/main/kotlin/net/postchain/vectordb/VectorDbEventProcessor.kt
@@ -4,10 +4,12 @@ import net.postchain.base.BaseBlockBuilderExtension
 import net.postchain.base.TxEventSink
 import net.postchain.base.data.BaseBlockBuilder
 import net.postchain.common.exception.ProgrammerMistake
+import net.postchain.common.exception.UserMistake
 import net.postchain.core.BlockEContext
 import net.postchain.core.TxEContext
 import net.postchain.gtv.Gtv
 import net.postchain.gtv.GtvArray
+import net.postchain.gtv.GtvType
 
 const val EVENT_STORE_VECTOR_NAME = "store_vector"
 const val EVENT_DELETE_VECTOR_NAME = "delete_vector"
@@ -30,6 +32,18 @@ class VectorDbEventProcessor(
     }
 
     private fun storeVectorEvent(ctxt: TxEContext, data: GtvArray) {
+        if (data.getSize() != 3) {
+            throw UserMistake("Invalid number of arguments")
+        }
+        if (data[0].type != GtvType.INTEGER) {
+            throw UserMistake("Invalid argument type for 'context'")
+        }
+        if (data[1].type != GtvType.STRING) {
+            throw UserMistake("Invalid argument type for 'vector'")
+        }
+        if (data[2].type != GtvType.INTEGER) {
+            throw UserMistake("Invalid argument type for 'id'")
+        }
         val context = data[0].asInteger()
         val vector = data[1].asString()
         val id = data[2].asInteger()
@@ -38,6 +52,15 @@ class VectorDbEventProcessor(
     }
 
     private fun deleteVectorEvent(ctxt: TxEContext, data: GtvArray) {
+        if (data.getSize() != 2) {
+            throw UserMistake("Invalid number of arguments")
+        }
+        if (data[0].type != GtvType.INTEGER) {
+            throw UserMistake("Invalid argument type for 'context'")
+        }
+        if (data[1].type != GtvType.INTEGER) {
+            throw UserMistake("Invalid argument type for 'id'")
+        }
         val context = data[0].asInteger()
         val id = data[1].asInteger()
 
