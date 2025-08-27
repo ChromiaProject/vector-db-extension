e4da015f2265391361778e5796ff9b9101f27114
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-08-27 19:54:11 +0200

    GTX module metadata

--- src/main/kotlin/net/postchain/gtx/extensions/vectordb/VectorDbGTXModule.kt
+++ src/main/kotlin/net/postchain/gtx/extensions/vectordb/VectorDbGTXModule.kt
@@ -8,10 +8,16 @@ import net.postchain.core.BlockchainConfiguration
 import net.postchain.core.EContext
 import net.postchain.gtv.Gtv
 import net.postchain.gtv.GtvFactory.gtv
+import net.postchain.gtv.GtvType
 import net.postchain.gtv.mapper.toObject
+import net.postchain.gtx.ArgumentMetadata
 import net.postchain.gtx.GTXModule
 import net.postchain.gtx.GTXModuleAware
+import net.postchain.gtx.GTXModuleMetadata
+import net.postchain.gtx.MetadataProvider
 import net.postchain.gtx.PostchainContextAware
+import net.postchain.gtx.QueryMetadata
+import net.postchain.gtx.ReturnMetadata
 import net.postchain.gtx.SimpleGTXModule
 import net.postchain.gtx.special.GTXSpecialTxExtension
 import java.math.BigDecimal
@@ -34,8 +40,8 @@ open class VectorDbGTXModule(
 ) : SimpleGTXModule<VectorDbGTXModuleContext>(
         VectorDbGTXModuleContext(databaseOperations), mapOf(), mapOf(
         VECTOR_DB_QUERY_CLOSEST_OBJECTS to Companion::queryClosestObjects,
-    )
-), PostchainContextAware {
+)
+), PostchainContextAware, MetadataProvider {
 
     private var chainId: Long? = null
 
@@ -47,7 +53,8 @@ open class VectorDbGTXModule(
 
             val context = args["context"]?.asInteger() ?: throw UserMistake("No context argument supplied")
             val vectorQuery = args["q_vector"]?.asString() ?: throw UserMistake("No q_vector argument supplied")
-            val maxDistance = BigDecimal(args["max_distance"]?.asString() ?: throw UserMistake("No max_distance argument supplied"))
+            val maxDistance = BigDecimal(args["max_distance"]?.asString()
+                    ?: throw UserMistake("No max_distance argument supplied"))
             val maxVectors = args["max_vectors"]?.asInteger()?.let {
                 if (it > moduleContext.vectorDbConfig.maxVectors) {
                     throw UserMistake("max_vectors ($it) exceeds the maximum of ${moduleContext.vectorDbConfig.maxVectors}")
@@ -61,13 +68,29 @@ open class VectorDbGTXModule(
             return if (queryTemplate == null) {
                 vectorResult
             } else {
-                val queryTemplateType = queryTemplate["type"]?.asString() ?: throw UserMistake("No type argument supplied to query_template")
+                val queryTemplateType = queryTemplate["type"]?.asString()
+                        ?: throw UserMistake("No type argument supplied to query_template")
                 val queryTemplateArgs = queryTemplate["args"]?.asDict() ?: mapOf()
                 return moduleContext.module.query(ctx, queryTemplateType,
                         gtv(mapOf("closest_results" to vectorResult) + queryTemplateArgs))
             }
         }
     }
+
+
+    override fun getMetadata() = GTXModuleMetadata(
+            operations = mapOf(),
+            queries = mapOf(VECTOR_DB_QUERY_CLOSEST_OBJECTS to QueryMetadata(
+                    args = listOf(
+                            ArgumentMetadata(name = "context", gtvTypes = setOf(GtvType.INTEGER)),
+                            ArgumentMetadata(name = "q_vector", gtvTypes = setOf(GtvType.STRING)),
+                            ArgumentMetadata(name = "max_distance", gtvTypes = setOf(GtvType.STRING), extendedType = "decimal"),
+                            ArgumentMetadata(name = "max_vectors", gtvTypes = setOf(GtvType.INTEGER), required = false),
+                            ArgumentMetadata(name = "query_template", gtvTypes = setOf(GtvType.DICT),
+                                    extendedType = "(type:text,args:map<text,gtv>)", required = false),
+                    ),
+                    returnType = ReturnMetadata(gtvTypes = setOf(GtvType.NULL, GtvType.BYTEARRAY, GtvType.STRING, GtvType.INTEGER, GtvType.DICT, GtvType.ARRAY, GtvType.BIGINTEGER))))
+    )
 
     override fun initializeContext(configuration: BlockchainConfiguration, postchainContext: PostchainContext) {
         conf.module = (configuration as GTXModuleAware).module
@@ -79,7 +102,7 @@ open class VectorDbGTXModule(
             logger.info { "VectorDB config: ${configuration.rawConfig["vector_db_extension"]?.asDict()}" }
 
             val ctx = postchainContext.blockBuilderStorage.openWriteConnection(chainId!!)
-            try{
+            try {
                 databaseOperations.initialize(ctx, conf.vectorDbConfig)
             } finally {
                 postchainContext.blockBuilderStorage.closeWriteConnection(ctx, true)
@@ -93,7 +116,7 @@ open class VectorDbGTXModule(
 
     override fun getSpecialTxExtensions() = emptyList<GTXSpecialTxExtension>()
 
-    override fun makeBlockBuilderExtensions(): List<BaseBlockBuilderExtension>  {
+    override fun makeBlockBuilderExtensions(): List<BaseBlockBuilderExtension> {
         return listOf(VectorDbEventProcessor(databaseOperations, conf.vectorDbConfig))
     }
 }
