adc2241b3990fd974c90559bddec0c42f86e7b6c
Johan Nilsson <johan.nilsson@chromaway.com>
2025-12-03 13:53:36 +0100

    Mocked embedding service in test, bumped chr for library chain deployment

---
+++ src/test/kotlin/net/postchain/gtx/extensions/vectordb/helpers/MockEmbeddingRestApi.kt
@@ -0,0 +1,55 @@
+package net.postchain.gtx.extensions.vectordb.helpers
+
+import net.postchain.gtx.extensions.vectordb.VLLMEmbeddingResponse
+import net.postchain.gtx.extensions.vectordb.VLLMEmbeddingResponseData
+import net.postchain.gtx.extensions.vectordb.vLLMEmbeddingRequest
+import net.postchain.gtx.extensions.vectordb.vLLMEmbeddingResponse
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.then
+import org.http4k.core.with
+import org.http4k.filter.ServerFilters
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.server.Http4kServer
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+import java.io.Closeable
+
+class MockEmbeddingRestApi : HttpHandler, Closeable {
+    private var server: Http4kServer? = null
+    var data = mapOf<String, String>()
+
+    private val app = ServerFilters.CatchLensFailure.then(
+            routes(
+                    "/v1/embeddings" bind Method.POST to { request ->
+                        val embeddingRequest = vLLMEmbeddingRequest(request)
+
+                        val responseData = VLLMEmbeddingResponse("id", System.currentTimeMillis(), embeddingRequest.model, listOf(
+                                VLLMEmbeddingResponseData(0, data[embeddingRequest.input[0]]!!
+                                        .removePrefix("[").removeSuffix("]").split(","))
+                        ))
+
+                        Response(Status.OK).with(vLLMEmbeddingResponse of responseData)
+                    },
+            )
+    )
+
+    override fun invoke(request: Request): Response = app(request)
+
+    fun start() {
+        server = app.asServer(SunHttp(0)).start()
+    }
+
+    fun port(): Int? {
+        return server?.port()
+    }
+
+    override fun close() {
+        server?.stop()
+        server = null
+    }
+}
--- src/test/kotlin/net/postchain/gtx/extensions/vectordb/manual/ManualVectorDbEmbeddingComputeTest.kt
+++ src/test/kotlin/net/postchain/gtx/extensions/vectordb/manual/ManualVectorDbEmbeddingComputeTest.kt
@@ -1,5 +1,6 @@
-package net.postchain.gtx.extensions.vectordb
+package net.postchain.gtx.extensions.vectordb.manual
 
+import net.postchain.gtx.extensions.vectordb.PGVectorBaseTest
 import assertk.assertThat
 import assertk.assertions.hasSize
 import assertk.assertions.isEqualTo
@@ -9,45 +10,40 @@ import net.postchain.devtools.query
 import net.postchain.gtv.GtvFactory.gtv
 import net.postchain.gtv.GtvNull
 import net.postchain.gtx.GtxOp
-import net.postchain.gtx.extensions.vectordb.helpers.MockEmbeddingRestApi
+import net.postchain.gtx.extensions.vectordb.helpers.SentenceTransformationMiniLMContainer
 import net.postchain.gtx.extensions.vectordb.helpers.buildTransaction
-import net.postchain.gtx.extensions.vectordb.helpers.generateVector
 import net.postchain.gtx.extensions.vectordb.helpers.queryClosestObjectsNoTemplate
 import net.postchain.images.directory1.awaitUntilAsserted
 import net.postchain.images.directory1.testLogger
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
 import java.util.concurrent.TimeUnit
 
+/**
+ * Manual test against a real model (smaller than we use on dev/testnet).
+ */
+@Disabled
 @Timeout(10, unit = TimeUnit.MINUTES)
-class VectorDbEmbeddingComputeIT : PGVectorBaseTest() {
+class ManualVectorDbEmbeddingComputeTest : PGVectorBaseTest() {
 
-    private val embeddingService = MockEmbeddingRestApi()
+    private val embeddingServiceContainer = SentenceTransformationMiniLMContainer()
 
     @BeforeEach
     fun beforeEach() {
-        embeddingService.start()
+        embeddingServiceContainer.start()
     }
 
     @AfterEach
     fun afterEach() {
-        embeddingService.close()
+        embeddingServiceContainer.stop()
     }
 
     @Test
     fun `compute multiple embeddings`() {
-        val text1 = "Blockchains are primitive and experimental compared with databases. Traditional blockchains record information in a list format, making it difficult to store and manage complex data sets on-chain. They are also reliant on centralized servers and third-party providers."
-        val text2 = "Chromia records structured blockchain data in linked relational tables. This innovation, which we call relational blockchain, makes it possible to query data directly on-chain, perform hundreds of read-and-write operations with a single transaction, and index block data in real-time."
-        val text3 = "Chromia CHR token is the native token designed to empower the Chromia platform and foster a mutually beneficial relationship between developers, users, and investors."
-        embeddingService.data = mapOf(
-                text1 to generateVector(384),
-                text2 to generateVector(384),
-                text3 to generateVector(384),
-        )
-
-        val embeddingServiceUrl = "http://localhost:${embeddingService.port()}"
+        val embeddingServiceUrl = "http://${embeddingServiceContainer.host}:${embeddingServiceContainer.getMappedPort(80)}"
         testLogger.info("Embedding service URL: $embeddingServiceUrl")
         configOverrides.setProperty("extension.vector_db.embedding.url", embeddingServiceUrl)
 
@@ -57,15 +53,15 @@ class VectorDbEmbeddingComputeIT : PGVectorBaseTest() {
         buildBlock(DEFAULT_CHAIN_IID, node.buildTransaction(listOf(
                 GtxOp("embed",
                         gtv("id-1"),
-                        gtv(text1)
+                        gtv("Blockchains are primitive and experimental compared with databases. Traditional blockchains record information in a list format, making it difficult to store and manage complex data sets on-chain. They are also reliant on centralized servers and third-party providers.")
                 ),
                 GtxOp("embed",
                         gtv("id-2"),
-                        gtv(text2)
+                        gtv("Chromia records structured blockchain data in linked relational tables. This innovation, which we call relational blockchain, makes it possible to query data directly on-chain, perform hundreds of read-and-write operations with a single transaction, and index block data in real-time.")
                 ),
                 GtxOp("embed",
                         gtv("id-3"),
-                        gtv(text3)
+                        gtv("Chromia CHR token is the native token designed to empower the Chromia platform and foster a mutually beneficial relationship between developers, users, and investors.")
                 ),
         )))
 
@@ -75,7 +71,7 @@ class VectorDbEmbeddingComputeIT : PGVectorBaseTest() {
             // Verify each embedding is stored in the vector db and can be queried with a distance of 0
             listOf("id-1", "id-2", "id-3").forEach { id ->
                 val textEmbedding = node.query(DEFAULT_CHAIN_IID) {
-                    it.query("get_text_embedding", gtv(mapOf("id" to gtv(id))))
+                    it.query("get_text_embedding", gtv(mapOf("id" to gtv(id), )))
                 }
 
                 assertThat(textEmbedding).isNotEqualTo(GtvNull)
